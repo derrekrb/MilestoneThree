9beddd9bdca2b587881073ccb13bab38329c530b
Matthew Palmer <palmermatthew645@gmail.com>
2021-04-18 22:18:11 -0600

    Help and Clear
    Added a help button to give information about the UVsim
    Added a clear input button so that users can clear all input and the memory

--- view.py
+++ view.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from tkinter import filedialog
 from model import Memory
 from controller import Controller
+from tkinter import messagebox
 
 
 class View:
@@ -16,7 +17,7 @@ class View:
         operation_code,
         operand,
         accumulator,
-        big_storage
+        big_storage,
     ):
         # Initialize variables from main
         # and initialize main window
@@ -69,6 +70,10 @@ class View:
         button_save = tk.Button(button_frame, text="Export File", command=self.saveFile)
         button_save.grid(row=0, column=1, padx=5)
 
+        # Create a frame for the help button
+        help_button = tk.Button(self.main_frame, text="Help", command=self.helpButton)
+        help_button.grid(row=0, column=2)
+
         # Create frame for input and initialize input boxes
         frame_input = tk.Frame(canvas)
         frame_input.grid(row=1, column=0)
@@ -97,7 +102,9 @@ class View:
         self.memory_frame.grid(column=1, row=1)
 
         # Create the Load Memory Button
-        self.load_memory_btn = tk.Button(self.main_frame, text="Load Memory")
+        self.load_memory_btn = tk.Button(
+            self.main_frame, text="Load Memory", command=self.loadMemory
+        )
         self.load_memory_btn.grid(row=2, columnspan=1, pady=10)
 
         # Create the Run Program Button
@@ -146,13 +153,18 @@ class View:
 
         register_frame.grid(row=1, column=2, padx=10, pady=10)
 
-        # Update memory when load Memory button is pressed
-        self.load_memory_btn.bind("<Button-1>", self.loadMemory)
+        clear_all = tk.Button(
+            self.main_frame, text="CLEAR ALL", command=self.clear_contents
+        )
+        clear_all.grid(row=2, column=2)
+
+        # # Update memory when load Memory button is pressed
+        # self.load_memory_btn.bind("<Button-1>", self.loadMemory)
 
         # Run the loaded Program when Run Program button is pressed
         self.run_program_btn.bind("<Button-1>", self.runProgram)
 
-    def loadMemory(self, event):
+    def loadMemory(self):
         """When load memory button is pressed, the memory is updated with new values in the entry and loaded into the memory frame"""
 
         # Checks if inputs are valid words in BASIC ml
@@ -195,7 +207,7 @@ class View:
             self.operation_code,
             self.operand,
             self.accumulator,
-            self.big_storage
+            self.big_storage,
         )
 
         # Return an array of updated values from the Controller
@@ -246,6 +258,31 @@ class View:
         with open(export, "w") as f:
             f.write(self.text_input.get("1.0", "end"))
 
+    def helpButton(self):
+        """ Suplies an overview of the UVsim"""
+        messagebox.showinfo(
+            "?",
+            f"""Overview: -The UVSim is a Graphical User Interface software simulator created by us over at IED (Innovative EDucation). Universities typically have a course where computer science students learn to work with registers and very low level coding, so this simulator allows for a simple and effective machine language environment. UVSIM works with a language called BasicML, which consists of instructions written in a signed four-digit decimal number, such as +1050, -5768, etc. UVSim has a 100-word memory, which are referenced by the last two digits of whatever command is entered. The BasicML program must be loaded into main memory starting at location 00 before running the program. The UVSim contains a CPU, register, and main memory. There is also an accumulator, which is what we will use to do different arithmetic or move around numbers in various ways. Each instruction (signed four-digit decimal number) occupies one word of the 100 word UVSim memory. Each slot may contain one of these instructions, or a data value that is placed inside one of the memory slots to be worked with.
+
+            The first two digits of each instruction are the operation code specifying the operation to be performed, which will be explained in the Features section.
+
+            The last two digits of each instruction are the operand, the address of the memory location containing the word we will be operating with.
+
+            Features: Here is a list of the operations that can be performed (Based on the first two digits of the signed four digit instruction). I/O operation:
+
+            READ = 10 Read a word from the keyboard into a specific location in memory. WRITE = 11 Write a word from a specific location in memory to screen. Load/store operations: LOAD = 20 Load a word from a specific location in memory into the accumulator. STORE = 21 Store a word from the accumulator into a specific location in memory. Arithmetic operation: Add = 30 Add a word from a specific location in memory to the word in the accumulator (leave the result in the accumulator) SUBTRACT = 31 Subtract a word from a specific location in memory from the word in the accumulator (leave the result in the accumulator) DIVIDE = 32 Divide the word in the accumulator by a word from a specific location in memory (leave the result in the accumulator). MULTIPLY = 33 multiply a word from a specific location in memory to the word in the accumulator (leave the result in the accumulator). Control operation: BRANCH = 40 Branch to a specific location in memory BRANCHNEG = 41 Branch to a specific location in memory if the accumulator is negative. BRANCHZERO = 42 Branch to a specific location in memory if the accumulator is zero. HALT = 43 Pause the program Ending the Program / Output: Along with all of these operations that can be performed, the UVSim will complete once it has either ran through all 100-word memory slots, or if the command -99999 is entered. Use -99999 if you can to end the program at a quicker speed. Once the input is done being read, it will be checked for validity to make sure there are no commands that are invalid and dont follow the signed four digit decimal number format. The program will be ran to completion and output the following: Registers:
+
+            Accumulator Instruction Counter (Counts the number of instructions ran in your program) Instruction Register (Contains the last instruction read by your program before completion) Operation Code (Contains the last operation code read by your program before completion) Operand (Contains the last operand read by your program before completion) Memory:
+
+            When you are storing an integer into a memory slot in the data range of 0-99, you are able to store an integer of size -9999 to 9999. When you are performing arithmetic, there is a higher range available of -999999 to 999999. Whenever an integer in a memory location is lower than -9999 or higher than 9999, the respective -9999 or +9999 are shown in the memory. However, when you pull the integer from those locations, it will contain the respective integer that is higher or lower than those values that had previously been stored. This is done to maintain simplicity in the formatting of the 100 slots of memory, along with hinting to the user that the memory location contains a number that is greater than 4 digits:
+
+            Memory will be printed out in a table format, with each of the instructions in the correct memory locations that they were assigned. If there was an integer number stored in a memory-word location, then it will be converted to the format that the instructions are. If the integer is negative, it will have a '-' sign in front, and if it is a positive integer it will have a '+' sign like the other instructions. Conclusion:
+
+            For testing perposes the input value that is required to run the read function for the user input is +0005.
+
+            All of this will be presented in a GUI type environment. We hope this provides a solid work environment for each of your students at your university/college and that it will help them come to a stronger understanding of how to work with registers, along with how to work with a low-level language like BasicML.""",
+        )
+
     def updateWindow(self, lyst):
         """Takes the array of outputs from the controller class and
         loads it into the Output array"""
@@ -272,4 +309,16 @@ class View:
                 self.retreive_mem_contents.append(mem_content)
                 memory_location += 1
         self.memory_frame.grid(column=1, row=1)
-        
+
+    def clear_contents(self):
+        """ Clears contents of input and memory"""
+
+        MsgBox = tk.messagebox.askquestion(
+            "Clear all",
+            "This will clear the contents of all input and memory:\nAre you sure you want to continue?",
+        )
+        if MsgBox == "yes":
+            # Clear contents of text for new content
+            self.text_input.delete("1.0", "end")
+
+            self.loadMemory()
